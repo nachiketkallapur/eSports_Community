992530af4144e5681db728c33ea11b841c1bf85e
NeilNagaraj <neilnagaraj1@gmail.com>
2020-11-13 20:54:23 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import FormInput from './form-input.component';
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import Register from './pages/register/register.component';
 function App() {
   return (
     <div className="App">
-      <FormInput/>
-
+      <Switch>
+        <Route exact={true} path="/" component={HomePage} />
+        <Route path='/register' component={Register} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/register-form/register-form.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+/*Type HEllo if you can see screen*/
+function RegisterForm(ownProps) {
+    console.log(ownProps.match);
+    const player = "Neil";
+    return (
+        <div>
+            <h1>Register Form for eSports</h1>
+        </div>
+    )
+}
+
+export default RegisterForm; 
