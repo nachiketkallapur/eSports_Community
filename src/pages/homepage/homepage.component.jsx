d14e8038532157155cb72aaaa6052cbe6df0b07c
nachiketkallapur <nachiketkallapur@gmail.com>
2020-12-05 00:53:07 +0530

    Adding header not complete

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     return (
         <div className='homepage'>
             <img className='game-image' src={gameImage} alt='game'/>
-            <h1 className='header'>Welcome to eSports Community</h1>
+            <h1 className='heading'>Welcome to eSports Community</h1>
         </div>
     )
 }
